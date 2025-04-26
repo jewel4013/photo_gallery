4c06f328fcbe681d19203fe7a6e148875a171334
jewel4013 <jewel.rana.4013@gmail.com>
2025-04-27 00:19:39 +0600

    Final

--- index.php
+++ index.php
@@ -1,16 +1,44 @@
 <?php
 include 'includes/header.php';
+
+$query = "SELECT * FROM images ORDER BY upload_date DESC";
+$stmt = $pdo->query($query);
+
+$images = $stmt->fetchAll();
+
 ?>
+
+
 <h1>This is Photo Gallery</h1>
-<div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Image Title</h5>
-        <p class="card-text">
-            Description
-        </p>        
-    </div>
+
+<div class="row">
+    <?php if(count($images) > 0){
+        foreach($images as $image){ ?>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/<?php echo $image['filename'] ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $image['title'] ?></h5>
+                    <p class="card-text">
+                        <?php echo $image['description'] ?>
+                    </p>
+                    <p>
+                        <?php echo date('d, M Y', strtotime($image['upload_date'])) ?>
+                    </p>        
+                </div>
+        </div>
+    <?php 
+        }        
+    }else{ ?>
+        <div class="alert alert-danger col-md-6" role="alert">
+            No Data Found.
+        </div>
+    <?php } ?>
+        
+
 </div>
+
+
+
 
 <?php
 include 'includes/footer.php';
